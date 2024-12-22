5cb390855e0dbb817201a1d7e165dbe380fc15fb
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-22 19:23:05 +0200

    feat(scripts): update logging

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-packages=("fastcrawl" "examples")
+packages=("fastcrawl/" "examples/")
 
 for package in "${packages[@]}"; do
-    echo "Isort formatting for $package..."
-    isort "$package/"
+    echo "Isort formatting for $package package..."
+    isort $package
 
-    echo "Autoflake formatting for $package..."
-    autoflake "$package/"
+    echo "Autoflake formatting for $package package..."
+    autoflake $package
 
-    echo "Black formatting for $package..."
-    black "$package/"
+    echo "Black formatting for $package package..."
+    black $package
 
-    echo "Flake8 checking for $package..."
-    flake8 "$package/"
+    echo "Flake8 checking for $package package..."
+    flake8 $package
 
-    echo "Pylint checking for $package..."
-    pylint "$package/"
+    echo "Pylint checking for $package package..."
+    pylint $package
 
-    echo "Mypy checking for $package..."
-    mypy "$package/"
+    echo "Mypy checking for $package package..."
+    mypy $package
 done
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-packages=("fastcrawl" "examples")
+packages=("fastcrawl/" "examples/")
 
 for package in "${packages[@]}"; do
-    echo "Isort checking for $package..."
-    isort "$package/" --check
+    echo "Isort checking for $package package..."
+    isort $package --check
 
-    echo "Autoflake checking for $package..."
-    autoflake "$package/" --check
+    echo "Autoflake checking for $package package..."
+    autoflake $package --check
 
-    echo "Black checking for $package..."
-    black "$package/" --check
+    echo "Black checking for $package package..."
+    black $package --check
 
-    echo "Flake8 checking for $package..."
-    flake8 "$package/"
+    echo "Flake8 checking for $package package..."
+    flake8 $package
 
-    echo "Pylint checking for $package..."
-    pylint "$package/"
+    echo "Pylint checking for $package package..."
+    pylint $package
 
-    echo "Mypy checking for $package..."
-    mypy "$package/"
+    echo "Mypy checking for $package package..."
+    mypy $package
 done

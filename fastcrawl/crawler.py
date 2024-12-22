e9e9e33476e881db5de717bbd9169f176a09b875
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-22 14:23:32 +0200

    feat: rewrite core logic of the crawler

--- fastcrawl/crawler.py
+++ fastcrawl/crawler.py
@@ -1,12 +1,11 @@
 import asyncio
 import logging
 from abc import ABC, abstractmethod
-from asyncio import Task
 from typing import Any, AsyncIterator
 
 from httpx import AsyncClient, Timeout
 
-from fastcrawl.models import Request, Response
+from fastcrawl.models import CrawlerConfig, Request, Response
 
 
 class Crawler(ABC):
@@ -14,36 +13,51 @@ class Crawler(ABC):
 
     Attributes:
         logger (logging.Logger): Logger for the crawler.
+        config (CrawlerConfig): Configuration for the crawler. Override it to set custom configuration.
 
     """
 
     logger: logging.Logger
-    _request_tasks: list[Task]
+    config: CrawlerConfig = CrawlerConfig()
+
+    _queue: asyncio.Queue
 
     def __init__(self) -> None:
         self.logger = logging.getLogger(self.__class__.__name__)
-        self._request_tasks = []
+        self._queue = asyncio.Queue()
 
     @abstractmethod
     async def generate_requests(self) -> AsyncIterator[Request]:
-        """Yields requests to be processed."""
+        """Yields requests to be handled."""
         if False:  # pylint: disable=W0125
             yield Request(url="https://example.com", callback=lambda _: None)  # just a stub for mypy
 
     async def run(self) -> None:
         """Runs the crawler."""
-        async for request in self.generate_requests():
-            self._create_request_task(request)
-        await asyncio.gather(*self._request_tasks)
+        self.logger.info("Running crawler with config:\n%s", self.config.model_dump_json(indent=2))
 
-    def _create_request_task(self, request: Request) -> None:
-        """Creates a task to handle the `request`."""
-        task = asyncio.create_task(self._handle_request(request))
-        self._request_tasks.append(task)
+        async for request in self.generate_requests():
+            await self._queue.put(request)
+
+        workers = [asyncio.create_task(self._worker()) for _ in range(self.config.workers)]
+        await self._queue.join()
+        for worker in workers:
+            worker.cancel()
+
+    async def _worker(self) -> None:
+        """Worker to handle requests from the queue."""
+        while True:
+            request = await self._queue.get()
+            try:
+                await self._handle_request(request)
+            except Exception as exc:  # pylint: disable=W0718
+                self.logger.error("Error handling request %s: %s", request, exc)
+            finally:
+                self._queue.task_done()
 
     async def _handle_request(self, request: Request) -> None:
         """Handles the `request`."""
-        self.logger.debug("Request: %s", request)
+        self.logger.info("Handling request: %s", request)
 
         async with AsyncClient() as client:
             request_kwargs: dict[str, Any] = {
@@ -61,16 +75,16 @@ class Crawler(ABC):
             if request.timeout is not None:
                 request_kwargs["timeout"] = Timeout(request.timeout.total_seconds())
             if request.follow_redirects is not None:
-                request_kwargs["allow_redirects"] = request.follow_redirects
+                request_kwargs["follow_redirects"] = request.follow_redirects
             httpx_response = await client.request(**request_kwargs)
 
         response = Response.from_httpx_response(httpx_response, request)
-        self.logger.debug("Response: %s", response)
+        self.logger.debug("Got response: %s", response)
 
         callback_result = request.callback(response)
         if hasattr(callback_result, "__aiter__"):
             async for item in callback_result:
                 if isinstance(item, Request):
-                    self._create_request_task(item)
+                    await self._queue.put(item)
         else:
             await callback_result

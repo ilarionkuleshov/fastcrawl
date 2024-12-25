0d91593b0df510e4b8fa1ee000bb77549a56eca6
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-25 22:07:45 +0200

    fix(tests): rename MockBasePipeline to MockPipeline

--- tests/test_base_pipeline.py
+++ tests/test_base_pipeline.py
@@ -3,7 +3,7 @@ import pytest
 from fastcrawl import BasePipeline
 
 
-class MockBasePipeline(BasePipeline[str]):
+class MockPipeline(BasePipeline[str]):
     """A mock class for testing the `BasePipeline` class."""
 
     async def process_item(self, item: str) -> str | None:
@@ -13,25 +13,25 @@ class MockBasePipeline(BasePipeline[str]):
 
 def test_logger() -> None:
     """Tests the `logger` attribute of the `BasePipeline` class."""
-    pipeline = MockBasePipeline()
-    assert pipeline.logger.name == "MockBasePipeline"
+    pipeline = MockPipeline()
+    assert pipeline.logger.name == "MockPipeline"
 
 
 def test_expected_type() -> None:
     """Tests the `_expected_type` attribute of the `BasePipeline` class."""
-    pipeline = MockBasePipeline()
+    pipeline = MockPipeline()
     assert pipeline._expected_type is str  # pylint: disable=W0212
 
 
 @pytest.mark.asyncio
 async def test_process_item_with_check() -> None:
     """Tests the `process_item_with_check` method of the `BasePipeline` class."""
-    pipeline = MockBasePipeline()
+    pipeline = MockPipeline()
     assert await pipeline.process_item_with_check("test") == await pipeline.process_item("test")
     assert await pipeline.process_item_with_check(1) == 1
 
 
 def test_str() -> None:
     """Tests the `__str__` method of the `BasePipeline` class."""
-    pipeline = MockBasePipeline()
-    assert str(pipeline) == "<MockBasePipeline[str]>"
+    pipeline = MockPipeline()
+    assert str(pipeline) == "<MockPipeline[str]>"

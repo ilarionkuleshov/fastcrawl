65bf1c3dcaccbc8fe0cd1c061059eb2d708226f8
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-25 22:06:11 +0200

    feat(tests): add tests for BasePipeline

---
+++ tests/test_base_pipeline.py
@@ -0,0 +1,37 @@
+import pytest
+
+from fastcrawl import BasePipeline
+
+
+class MockBasePipeline(BasePipeline[str]):
+    """A mock class for testing the `BasePipeline` class."""
+
+    async def process_item(self, item: str) -> str | None:
+        """See `BasePipeline` class."""
+        return item * 2
+
+
+def test_logger() -> None:
+    """Tests the `logger` attribute of the `BasePipeline` class."""
+    pipeline = MockBasePipeline()
+    assert pipeline.logger.name == "MockBasePipeline"
+
+
+def test_expected_type() -> None:
+    """Tests the `_expected_type` attribute of the `BasePipeline` class."""
+    pipeline = MockBasePipeline()
+    assert pipeline._expected_type is str  # pylint: disable=W0212
+
+
+@pytest.mark.asyncio
+async def test_process_item_with_check() -> None:
+    """Tests the `process_item_with_check` method of the `BasePipeline` class."""
+    pipeline = MockBasePipeline()
+    assert await pipeline.process_item_with_check("test") == await pipeline.process_item("test")
+    assert await pipeline.process_item_with_check(1) == 1
+
+
+def test_str() -> None:
+    """Tests the `__str__` method of the `BasePipeline` class."""
+    pipeline = MockBasePipeline()
+    assert str(pipeline) == "<MockBasePipeline[str]>"

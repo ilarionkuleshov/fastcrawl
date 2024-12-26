cee7ba82dd3697d805b0cf4d14925aaa3139211f
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-27 01:16:51 +0200

    feat: update test script and coverage config

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-coverage run -m pytest tests/
-coverage html
+generate_html_report=true
+coverage_file=".coverage"
+
+for option in "$@"; do
+    case $option in
+    --generate-html-report=*)
+        generate_html_report="${option#*=}"
+        shift
+        ;;
+    --coverage-file=*)
+        coverage_file="${option#*=}"
+        shift
+        ;;
+    *)
+        echo "Unknown option: $option"
+        exit 1
+        ;;
+    esac
+done
+
+coverage run --data-file=$coverage_file -m pytest tests/
+
+if [ "$generate_html_report" == "true" ]; then
+    coverage html --data-file=$coverage_file
+fi
